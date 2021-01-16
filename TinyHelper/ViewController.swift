02743313398d783a91064395f0137c6c65c43dcb
Nancy Wu <nancywu@macbook-pro-8.local>
2021-01-16 18:47:53 -0500

    Initial Commit

---
+++ TinyHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinyHelper
+//
+//  Created by Nancy Wu on 2021-01-16.
+//  Copyright © 2021 Nancy Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
